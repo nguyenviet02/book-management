de14d245ede32163cac52b11b14856ae7678d54c
Nguyen Viet <nguyenviet02.dev@gmail.com>
2023-12-24 14:58:34 +0700

    add page qlsach

--- app.js
+++ app.js
@@ -6,16 +6,18 @@ const handlebars = require("express-handlebars");
 dotenv.config({ path: "./config.env" });
 const app = express();
 
+const qlsach = require("./routers/qlsach");
+
 if (process.env.NODE_ENV === "development") {
-  app.use(morgan("dev"));
+	app.use(morgan("dev"));
 }
 
 // template engine
 app.engine(
-  "hbs",
-  handlebars.engine({
-    extname: ".hbs",
-  })
+	"hbs",
+	handlebars.engine({
+		extname: ".hbs",
+	})
 );
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "views"));
@@ -25,8 +27,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
+// routers
+app.use("/qlsach", qlsach);
+
 app.get("/", (req, res) => {
-  res.render("home");
+	res.render("home");
 });
 
 module.exports = app;
